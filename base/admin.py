4050358e5cbdb163b223823b52cc1c9d241d8e55
tnguyen11211 <tnguyen11211@gmail.com>
2023-11-20 16:06:11 -0800

    registered test requirements to admin

--- base/admin.py
+++ base/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, Profile, Attendance, Tournament, TeachingHours, ServiceHours, PracticalScore
 
+admin.site.register(Profile)
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
+admin.site.register(Attendance)
+admin.site.register(Tournament)
+admin.site.register(TeachingHours)
+admin.site.register(ServiceHours)
+admin.site.register(PracticalScore)
