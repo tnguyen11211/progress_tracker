5a9d4f930c817567a440f32bb694d72f0a26de00
tnguyen11211 <tnguyen11211@gmail.com>
2023-12-05 23:40:21 -0800

    filter model objects to current quarter

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -8,12 +9,58 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from .forms import RoomForm, UserForm, ProfileForm
-from .models import Message, Room, Topic, Profile, Attendance, Tournament, ServiceHours, TeachingHours, PracticalScore
+from .models import Message, Room, Topic, Profile, Tournament, LeadershipHours, PracticalScore
+
+def getRecentDates(model):
+    if timezone.now().month <= 3:
+        dates = model.filter(date__month__range=['01', '03'], date__year=timezone.now().year)
+    elif timezone.now().month > 3 and timezone.now().month <= 6:
+        dates = model.filter(date__month__range=['04', '06'], date__year=timezone.now().year)
+    elif timezone.now().month > 6 and timezone.now().month <= 9:
+        dates = model.filter(date__month__range=['07', '09'], date__year=timezone.now().year)
+    elif timezone.now().month > 9 and timezone.now().month <= 12:
+        dates = model.filter(date__month__range=['10', '12'], date__year=timezone.now().year)
+    else:
+        dates = model.all()
+
+    return dates
+
+def setProfileStats():
+    profiles = Profile.objects.all()
+    tournaments = getRecentDates(Tournament.objects)
+    hours = getRecentDates(LeadershipHours.objects)
+    practicals = getRecentDates(PracticalScore.objects)
+
+    for profile in profiles:
+        profile.current_attendances = getRecentDates(profile.attendances).count()
+
+        tournament_count = 0
+        for tournament in tournaments:
+            if profile == tournament.profile:
+                tournament_count += 1
+        profile.current_tournaments = tournament_count
+
+        hours_count = 0
+        for h in hours:
+            if profile == h.profile:
+                hours_count += h.hours
+        profile.current_hours = hours_count
+
+        for practical in practicals:
+            if profile == practical.profile:
+                profile.current_score = practical.score
+
+        profile.save()
 
 def home(request):
-    students = Profile.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    context = {"students": students}
+    profiles = Profile.objects.filter(
+        Q(user__username__icontains=q) |
+        Q(name__icontains=q)
+    )
+        
+    context = {'profiles': profiles}
     return render(request, 'base/home.html', context)
 
 def loginPage(request):
@@ -53,6 +100,8 @@ def registerPage(request):
             user = form.save(commit=False)
             # change or clean form data here
             user.save()
+            user.profile.name = user.username
+            user.save()
             login(request, user)
             return redirect('home')
         else:
@@ -76,7 +125,10 @@ def rooms(request):
     room_count_all = Room.objects.all().count()
     room_count = rooms.count()
     room_messages = Message.objects.filter(
-        Q(room__topic__name__icontains=q))[0:5]
+        Q(room__topic__name__icontains=q) | 
+        Q(room__name__icontains=q) |
+        Q(room__description__icontains=q)
+        )[0:5]
 
     context = {'rooms': rooms, 'topics': topics,
                'room_count': room_count, 'room_count_all': room_count_all, 'room_messages': room_messages}
@@ -104,29 +156,28 @@ def room(request, pk):
 
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
-    profile = Profile.objects.filter(user=user)
+    profile = user.profile
     rooms = user.room_set.all()
     room_messages = user.message_set.all()
     room_count_all = Room.objects.all().count()
     topics = Topic.objects.all()
 
-    attendances = Attendance.objects.filter(profile__in=profile)
-    tournaments = Tournament.objects.filter(profile__in=profile)
-    teaching_hours = TeachingHours.objects.filter(profile__in=profile)
-    service_hours = ServiceHours.objects.filter(profile__in=profile)
-    practical_score = PracticalScore.objects.filter(profile__in=profile)
+    attendances = profile.attendances.all()
+    tournaments = Tournament.objects.filter(profile=profile)
+    hours = LeadershipHours.objects.filter(profile=profile)
+    practical_scores = PracticalScore.objects.filter(profile=profile)
 
-    context = {'user': user, 
+    context = {'user': user,
                'rooms': rooms,
                'room_messages': room_messages, 
                'room_count_all': room_count_all, 
                'topics': topics,
                'attendances': attendances,
                'tournaments': tournaments,
-               'teaching_hours': teaching_hours,
-               'service_hours': service_hours,
-               'practical_scores': practical_score
+               'hours': hours,
+               'practical_scores': practical_scores
                }
+    
     return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
@@ -215,7 +266,13 @@ def updateUser(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            setProfileStats()
             return redirect('user-profile', pk=user.id)
+        else:
+            for error in user_form.errors:
+                messages.error(request, user_form.errors.get(error))
+            for error in profile_form.errors:
+                messages.error(request, profile_form.errors.get(error))
 
     return render(request, 'base/update-user.html', {'user_form': user_form, 'profile_form': profile_form})
 
