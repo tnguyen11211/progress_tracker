75ba74665c0c7071ef4ee0744e32c5a0df3da8f9
tnguyen11211 <tnguyen11211@gmail.com>
2024-01-09 17:20:17 -0800

    reworked current values in models

--- base/models.py
+++ base/models.py
@@ -1,16 +1,21 @@
-from django.utils import timezone
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 from PIL import Image
 
-class Attendance(models.Model):
-    date = models.DateField(default=timezone.now)
+def getRecentDates(model, profile):
+    if timezone.now().month <= 3:
+        dates = model.objects.filter(date__month__range=['01', '03'], date__year=timezone.now().year, profile=profile)
+    elif timezone.now().month > 3 and timezone.now().month <= 6:
+        dates = model.objects.filter(date__month__range=['04', '06'], date__year=timezone.now().year, profile=profile)
+    elif timezone.now().month > 6 and timezone.now().month <= 9:
+        dates = model.objects.filter(date__month__range=['07', '09'], date__year=timezone.now().year, profile=profile)
+    elif timezone.now().month > 9 and timezone.now().month <= 12:
+        dates = model.objects.filter(date__month__range=['10', '12'], date__year=timezone.now().year, profile=profile)
+    else:
+        dates = model.objects.filter(profile=profile)
 
-    def __str__(self):
-        return self.date.strftime("%b %d, %Y")
-    
-    class Meta:
-        ordering = ['-date']
+    return dates
 
 class Profile(models.Model):
     BROWN = 'Brown'
@@ -36,18 +41,47 @@ class Profile(models.Model):
     )
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=30, null=True)
     about = models.TextField(null=True, blank=True)
     picture = models.ImageField(default="default.svg", upload_to='profile_pictures', null=True)
     rank = models.CharField(max_length=20, choices=BELT_RANKS, null=True, blank=True)
     last_promoted = models.DateField(default=timezone.now)
 
-    attendances = models.ManyToManyField(Attendance, related_name='attendances')
+    @property
+    def current_attendances(self):
+        return getRecentDates(Attendance, self).count()
     
-    current_attendances = models.IntegerField(default=0, null=True)
-    current_tournaments = models.IntegerField(default=0, null=True)
-    current_hours = models.IntegerField(default=0, null=True)
-    current_score = models.IntegerField(default=0, null=True)
+    @property
+    def current_tournaments(self):
+        return getRecentDates(Tournament, self).count()
+    
+    @property
+    def current_leadership_hours(self):
+        leadership_hours = getRecentDates(LeadershipHours, self)
+        current_hours = 0
+        for h in leadership_hours:
+            current_hours += h.hours
+        return current_hours
+    
+    @property
+    def current_practical_score(self):
+        practical_scores =  getRecentDates(PracticalScore, self)
+        if not practical_scores:
+            return 0
+        else:
+            return practical_scores.first().score
+        
+    def get_recent_attendances(self):
+        return getRecentDates(Attendance, self)
+    
+    def get_recent_tournaments(self):
+        return getRecentDates(Tournament, self)
+    
+    def get_recent_leadership_hours(self):
+        return getRecentDates(LeadershipHours, self)
+    
+    def get_recent_practical_scores(self):
+        return getRecentDates(PracticalScore, self)
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
@@ -66,32 +100,48 @@ class Profile(models.Model):
                 # overwrite the larger image
                 img.save(self.picture.path)
 
+class Attendance(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name='attendance')
+    date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.__class__.__name__ + " for " + self.profile.name + " on " + self.date.strftime("%b %d, %Y")
+    
+    class Meta:
+        ordering = ['-date']
+
 class Tournament(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=200, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name='tournament')
+    event = models.CharField(max_length=30, null=True)
     date = models.DateField(default=timezone.now)
     
     def __str__(self):
-        return self.name
-
+        return self.__class__.__name__ + " for " + self.profile.name + " for " + self.event + " on " + self.date.strftime("%b %d, %Y")
+    
+    class Meta:
+        ordering = ['-date', 'event']
+    
 class LeadershipHours(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
-    event = models.CharField(max_length=200)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name='leadership_hour')
+    event = models.CharField(max_length=30)
     date = models.DateField(default=timezone.now)
     hours = models.IntegerField(null=True)
 
     def __str__(self):
-        string = self.event + " : " + self.date.strftime("%b %d, %Y") + " : " + str(self.hours)
+        string = self.__class__.__name__ + " for " + self.profile.name + " for " + self.event + " on " + self.date.strftime("%b %d, %Y") + " for " + str(self.hours) + " hours "
         return string
-    
+
 class PracticalScore(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name='practical_score')
     date = models.DateField(default=timezone.now)
     score = models.IntegerField(null=True)
 
     def __str__(self):
-        string = self.date.strftime("%b %d, %Y") + " : " + str(self.score)
+        string = self.__class__.__name__ + " for " + self.profile.name + " on " + self.date.strftime("%b %d, %Y") + " with score " + str(self.score)
         return string
+    
+    class Meta:
+        ordering = ['-date']
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
