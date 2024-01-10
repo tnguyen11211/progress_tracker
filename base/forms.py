c7512683e22341ddb9820fe731a5a14dc425dbb0
tnguyen11211 <tnguyen11211@gmail.com>
2024-01-09 17:20:44 -0800

    added forms for each model

--- base/forms.py
+++ base/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 from django.utils import timezone
 
-from .models import Room, Profile
+from .models import Room, Profile, Attendance, Tournament, LeadershipHours, PracticalScore
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -25,4 +25,36 @@ class ProfileForm(ModelForm):
         fields = ['picture', 'name', 'rank', 'last_promoted', 'about']
         widgets = {
             'last_promoted': DateInput(attrs=dict(max = timezone.localdate)),
+        }
+
+class AttendanceForm(ModelForm):
+    class Meta:
+        model = Attendance
+        fields = ['date']
+        widgets = {
+            'date': DateInput(attrs=dict(max = timezone.localdate)),
+        }
+
+class TournamentForm(ModelForm):
+    class Meta:
+        model = Tournament
+        fields = ['event', 'date']
+        widgets = {
+            'date': DateInput(attrs=dict(max = timezone.localdate)),
+        }
+
+class LeadershipHoursForm(ModelForm):
+    class Meta:
+        model = LeadershipHours
+        fields = ['event', 'date', 'hours']
+        widgets = {
+            'date': DateInput(attrs=dict(max = timezone.localdate)),
+        }
+
+class PracticalScoreForm(ModelForm):
+    class Meta:
+        model = PracticalScore
+        fields = ['date', 'score']
+        widgets = {
+            'date': DateInput(attrs=dict(max = timezone.localdate)),
         }
