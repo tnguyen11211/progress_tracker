c37cf2aee8c78417c36c9028fb363b6d5d66eba3
tnguyen11211 <tnguyen11211@gmail.com>
2024-01-11 17:56:27 -0800

    changed secret key

--- progress_tracker/settings.py
+++ progress_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1-#3@09$m8c)ssf=fdi285#b%mb_e50q6+ofth)3_v9v1x81vx'
+#SECRET_KEY = 'django-insecure-1-#3@09$m8c)ssf=fdi285#b%mb_e50q6+ofth)3_v9v1x81vx'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-1-#3@09$m8c)ssf=fdi285#b%mb_e50q6+ofth)3_v9v1x81vx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 CSRF_TRUSTED_ORIGINS = [
     'https://progress.bluedragonacademy.com',
