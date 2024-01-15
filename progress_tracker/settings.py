0617062833c6a60116371b8e6caf1210a28e2da1
tnguyen11211 <tnguyen11211@gmail.com>
2024-01-15 12:51:19 -0800

    use dj_database_url

--- progress_tracker/settings.py
+++ progress_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -100,15 +101,10 @@ WSGI_APPLICATION = 'progress_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': '3C2AEd3EfggDE-FB215c4c2bfaDCcE-C',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '40352',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
