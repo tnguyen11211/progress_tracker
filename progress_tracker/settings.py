4b3f511c327364d659ae9f4dab443de1c89527ff
tnguyen11211 <tnguyen11211@gmail.com>
2024-01-15 14:27:38 -0800

    static root path

--- progress_tracker/settings.py
+++ progress_tracker/settings.py
@@ -152,9 +152,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = BASE_DIR / 'static/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
