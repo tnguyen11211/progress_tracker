731832ea6456f05b09ccb29d6c47e179a90ef930
tnguyen11211 <tnguyen11211@gmail.com>
2024-03-07 18:49:37 -0800

    revert

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn progress_tracker.wsgi -b 0.0.0.0:8000
+web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn progress_tracker.wsgi
--- progress_tracker/settings.py
+++ progress_tracker/settings.py
@@ -148,13 +148,18 @@ USE_TZ = True
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+MEDIA_URL = '/images/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
+
+""" STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') """
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
